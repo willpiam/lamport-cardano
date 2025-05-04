056a95ef79625eec2c65a6bb3d73ee942111cf2d
willpiam <willdoyle422@gmail.com>
2025-05-04 17:07:24 -0400

    build datum

--- manystep_test.ts
+++ manystep_test.ts
@@ -1,4 +1,4 @@
-import { applyParamsToScript, Data, Emulator, fromText, generateEmulatorAccount, getAddressDetails, Lucid, MintingPolicy, mintingPolicyToId, paymentCredentialOf, scriptFromNative, SpendingValidator, validatorToAddress } from "npm:@lucid-evolution/lucid";
+import { applyParamsToScript, Constr, Data, Emulator, fromText, generateEmulatorAccount, getAddressDetails, Lucid, MintingPolicy, mintingPolicyToId, paymentCredentialOf, scriptFromNative, SpendingValidator, toHex, validatorToAddress } from "npm:@lucid-evolution/lucid";
 import blueprint from "./lamport-validator/plutus.json" with { type: "json" };
 import {
   assertEquals,
@@ -6,6 +6,7 @@ import {
   assertExists,
   assertRejects,
 } from "@std/assert";
+import { generateSeed } from "./Lamport.ts";
 
 // Setup test accounts
 const alice = generateEmulatorAccount({
@@ -34,14 +35,30 @@ const validator : SpendingValidator = {
 }
 const scriptAddress = validatorToAddress("Custom", validator, getAddressDetails(aliceAddress).stakeCredential);
 
+const MintAction = {
+  Mint: Data.to(new Constr(0, [])),
+  Burn: Data.to(new Constr(1, [])),
+};
+
+const State = {
+    Initial: (tokensNotInitialized: BigInt, publicKeyMerkleRoot: Uint8Array) => Data.to(new Constr(0, [tokensNotInitialized.toString(), toHex(publicKeyMerkleRoot)])),
+    Default: () => new Constr(1, []),
+}
+
+
 
 Deno.test("Mint our 8 tokens", async () => {
 
-    // const tx = await lucid.newTx()
-    //     .mintAssets(assetsToMint, Data.void())
-    //     .attach.MintingPolicy(mintingPolicy)
-    //     .pay.ToContract(scriptAddress, {kind: "inline", value: Data.void()}, assetsToMint)
-    //     .complete();
+    const seed = await generateSeed();
+    console.log(`seed: ${seed}`);
+    const initialState = State.Initial(8n, seed);
+    console.log(initialState);
+
+    const tx = await lucid.newTx()
+        .mintAssets(assetsToMint, MintAction.Mint)
+        .attach.MintingPolicy(mintingPolicy)
+        .pay.ToContract(scriptAddress, {kind: "inline", value: initialState}, assetsToMint)
+        .complete();
 
    
     
